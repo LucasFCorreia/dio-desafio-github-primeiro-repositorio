409d1faa5f48cbd5dc7916648b647ded49cf8d04
Lucas Correia <lucascorreia800@gmail.com>
2026-03-12 12:04:53 -0300

    primeiro commit

---
+++ logica-programacao/string-number-bool.js
@@ -0,0 +1,37 @@
+//pokemon
+let nomePokemon = "Pikachu"
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+let sexoPokemon = "M"
+let selecionavel = true
+
+
+//nome, idade, número de telefone, cpf, endereço, recebe pensão
+//string = armazenar texto
+//number = armazenar números
+//boolean = armazenar valores lógicos (true ou false)
+
+let nomePessoa = "Josicreide Do Pythinho"
+let idade = 115
+let numeroDeTelefone = "11999999999"
+let cpf = "12345678900"
+let endereco = "Rua dos Bobos, número 0, bairro da Jaguatirica"
+let recebePensao = true
+
+let pokemon = ["Pikachu", "Charmander", "Squirtle", "Bulbasaur"]
+
+pokemon.pop()
+
+console.log(pokemon)
+
+let timePokemon = [
+    ["pikachu", "M", 20, 45],
+    ["charmander", "F", 18, 40],
+    ["squirtle", "M", 22, 50],
+    ["bulbasaur", "F", 19, 48]
+]
+
+console.log("O pokemon " + timePokemon[1][0] + " é do sexo " + timePokemon[1][1] + " e está no nível " + timePokemon[1][2])
+
+
+
